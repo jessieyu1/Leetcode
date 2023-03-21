3f0c00a118a8ce62bdf5a6e5cee56c4176d7700d
Jessie Yu <61171021+jessieyu1@users.noreply.github.com>
2023-03-21 14:01:36 +1100

    optimise 217

--- 0217-contains-duplicate/0217-containsDuplicate.js
+++ 0217-contains-duplicate/0217-containsDuplicate.js
@@ -5,11 +5,8 @@
 var containsDuplicate = function (nums) {
   var set = new Set();
   for (var i = 0; i < nums.length; i++) {
-    if (!set.has(nums[i])) {
-      set.add(nums[i]);
-    } else {
-      return true;
-    }
+    if (set.has(nums[i])) return true;
+    set.add(nums[i]);
   }
   return false;
 };
