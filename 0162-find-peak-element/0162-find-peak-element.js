3f4eef57171d26580339e333515e935779f368fb
Jessie <yujiaxin2009@gmail.com>
2023-11-05 09:55:00 +1100

    fix edge case

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -2,11 +2,14 @@ function findPeakElement(nums) {
   let left = 0;
   let right = nums.length - 1;
 
+  if (nums[0] > nums[1]) return 0;
+
   while (left < right) {
     let mid = left + Math.floor((right - left) / 2);
+
     if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid;
-    if (nums[mid] < nums[left]) {
-        right = mid;
+    if (nums[mid] < nums[mid - 1]) {
+      right = mid;
     } else {
       left = mid + 1;
     }
--- 0162-find-peak-element/0162-find-peak-element.test.js
+++ 0162-find-peak-element/0162-find-peak-element.test.js
@@ -1,33 +1,51 @@
-const findPeakElement = require('./0162-find-peak-element');
+const findPeakElement = require("./0162-find-peak-element");
 
-let nums = [1,2,3,1]
-let expectedOutput = 2
+let nums = [1, 2, 3, 1];
+let expectedOutput = 2;
 
-let actualOutput  = findPeakElement(nums)
+let actualOutput = findPeakElement(nums);
 
 console.assert(
-    JSON.stringify(actualOutput) === JSON.stringify(expectedOutput),
-    `Expected ${expectedOutput}, but got ${actualOutput} instead`
+  JSON.stringify(actualOutput) === JSON.stringify(expectedOutput),
+  `Expected ${expectedOutput}, but got ${actualOutput} instead`
 );
 
-nums = [1,2,1,3,5,6,4]
+nums = [1, 2, 1, 3, 5, 6, 4];
 
-expectedOutput = 5
+expectedOutput = 5;
 
-actualOutput  = findPeakElement(nums)
+actualOutput = findPeakElement(nums);
 
 console.assert(
-    JSON.stringify(actualOutput) === JSON.stringify(expectedOutput),
-    `Expected ${expectedOutput}, but got ${actualOutput} instead`
+  JSON.stringify(actualOutput) === JSON.stringify(expectedOutput),
+  `Expected ${expectedOutput}, but got ${actualOutput} instead`
 );
 
-nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-expectedOutput = 14
+expectedOutput = 14;
 
-actualOutput  = findPeakElement(nums)
+actualOutput = findPeakElement(nums);
 
 console.assert(
-    JSON.stringify(actualOutput) === JSON.stringify(expectedOutput),
-    `Expected ${expectedOutput}, but got ${actualOutput} instead`
+  JSON.stringify(actualOutput) === JSON.stringify(expectedOutput),
+  `Expected ${expectedOutput}, but got ${actualOutput} instead`
+);
+
+nums = [2, 1];
+
+expectedOutput = 0;
+actualOutput = findPeakElement(nums);
+console.assert(
+  JSON.stringify(actualOutput) === JSON.stringify(expectedOutput),
+  `Expected ${expectedOutput}, but got ${actualOutput} instead`
+);
+
+nums = [1, 2,1,3,4,6,7];
+
+expectedOutput = 6;
+actualOutput = findPeakElement(nums);
+console.assert(
+  JSON.stringify(actualOutput) === JSON.stringify(expectedOutput),
+  `Expected ${expectedOutput}, but got ${actualOutput} instead`
 );
