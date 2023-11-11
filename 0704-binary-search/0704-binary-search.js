c22f4ef20de338bdc834df7d502e4ed9a88c40d5
Jessie <yujiaxin2009@gmail.com>
2023-11-11 19:44:06 +1100

    change interval

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -1,14 +1,13 @@
 search = function (nums, target) {
   let left = 0;
-  let right = nums.length - 1;
-
-  while (left <= right) {
-    let mid = Math.floor((right - left) / 2 + left);
+  let right = nums.length;
+  while (left < right) {
+    let mid = left + Math.floor((right - left) / 2);
     if (nums[mid] === target) return mid;
-    else if (target > nums[mid]) {
+    else if (nums[mid] > target) {
+      right = mid;
+    } else if (nums[mid] < target) {
       left = mid + 1;
-    } else {
-      right = mid - 1;
     }
   }
   return -1;
