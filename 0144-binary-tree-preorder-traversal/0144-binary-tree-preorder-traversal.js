72c14e54a2b04ee3a235a52524a9343bf0f678dc
Jessie <yujiaxin2009@gmail.com>
2023-11-28 07:25:29 +1100

    pre order traversal

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.js
@@ -0,0 +1,25 @@
+/**
+    Definition for a binary tree node.
+    function TreeNode(val, left, right) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+const preorderTraversal = function(root) {
+    let res = [];
+    const dst = ( root ) => {
+        if (root === null) return;
+        res.push(root.val);
+        dst(root.left);
+        dst(root.right);
+    }
+    dst(root);
+    return res;
+};
+
+module.exports = preorderTraversal;
