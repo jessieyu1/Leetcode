7d5853b959340d715f227dfb8bb933efa2cbf643
Jessie <yujiaxin2009@gmail.com>
2024-01-02 13:34:58 +0800

    optimise test cases

--- 0035-search-insert-position/0035-searchInsertPosition.test.js
+++ 0035-search-insert-position/0035-searchInsertPosition.test.js
@@ -1,5 +1,5 @@
 // Import the function to test
-const searchInsert = require("./0035-searchInsertPosition");
+const searchInsert = require("./practice");
 
 // Test case 1
 let nums = [1, 3, 5, 6];
@@ -8,7 +8,7 @@ let expectedOutput = 2;
 let actualOutput = searchInsert(nums, target);
 console.assert(
   JSON.stringify(actualOutput) === JSON.stringify(expectedOutput),
-  `Expected ${expectedOutput}, but got ${actualOutput} instead`
+  `Expected test case 1 to be ${expectedOutput}, but got ${actualOutput} instead`
 );
 // Test case 2
 nums = [1, 3, 5, 6];
@@ -17,7 +17,7 @@ expectedOutput = 1;
 actualOutput = searchInsert(nums, target);
 console.assert(
   JSON.stringify(actualOutput) === JSON.stringify(expectedOutput),
-  `Expected ${expectedOutput}, but got ${actualOutput} instead`
+  `Expected test case 2 to be ${expectedOutput}, but got ${actualOutput} instead`
 );
 
 // Test case 3
@@ -27,7 +27,7 @@ expectedOutput = 4;
 actualOutput = searchInsert(nums, target);
 console.assert(
   JSON.stringify(actualOutput) === JSON.stringify(expectedOutput),
-  `Expected ${expectedOutput}, but got ${actualOutput} instead`
+  `Expected test case 3 to be ${expectedOutput}, but got ${actualOutput} instead`
 );
 // Test case 4
 nums = [1, 3];
@@ -36,5 +36,5 @@ expectedOutput = 1;
 actualOutput = searchInsert(nums, target);
 console.assert(
   JSON.stringify(actualOutput) === JSON.stringify(expectedOutput),
-  `Expected ${expectedOutput}, but got ${actualOutput} instead`
+  `Expected test case 4 to be ${expectedOutput}, but got ${actualOutput} instead`
 );
