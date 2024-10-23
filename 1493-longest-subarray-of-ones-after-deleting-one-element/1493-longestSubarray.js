afe3bbb449bb09d870b23db4535829fd85baa13b
Jessie <yujiaxin2009@gmail.com>
2024-10-23 18:46:59 +1100

    remove unnecessary

--- 1493-longest-subarray-of-ones-after-deleting-one-element/1493-longestSubarray.js
+++ 1493-longest-subarray-of-ones-after-deleting-one-element/1493-longestSubarray.js
@@ -3,8 +3,6 @@ function longestSubarray(nums) {
   let left = 0;
   let eleToIndex = new Map();
 
-  if (!nums.includes(0)) return nums.length - 1;
-
   for (let right = 0; right < nums.length; right++) {
     eleToIndex.set(nums[right], (eleToIndex.get(nums[right]) || 0) + 1);
     while (eleToIndex.get(nums[right]) > 1 && nums[right] === 0) {
