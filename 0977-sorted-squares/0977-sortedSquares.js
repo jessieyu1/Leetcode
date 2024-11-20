375c9c0da052555606cbb9433a314998a4380d97
Jessie <yujiaxin2009@gmail.com>
2024-11-20 20:12:04 +1100

    update 977 sorted squares

--- 0977-sorted-squares/0977-sortedSquares.js
+++ 0977-sorted-squares/0977-sortedSquares.js
@@ -1,13 +1,9 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
-var sortedSquares = function (nums) {
-  var left = 0;
-  var right = nums.length - 1;
-  var newNum = [];
-  for (var i = nums.length - 1; i >= 0; i--) {
-    var square = 0;
+function sortedSquares(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  let newNum = [];
+  for (let i = nums.length - 1; i >= 0; i--) {
+    let square = 0;
     if (Math.abs(nums[left]) < Math.abs(nums[right])) {
       square = Math.abs(nums[right]);
       right--;
--- 0977-sorted-squares/0977-sortedSquares.test.js
+++ 0977-sorted-squares/0977-sortedSquares.test.js
@@ -6,7 +6,7 @@ let expectedOutput = [0, 1, 9, 16, 100];
 let actualOutput = sortedSquares(nums);
 console.assert(
   JSON.stringify(actualOutput) === JSON.stringify(expectedOutput),
-  `Expected ${expectedOutput}, but got ${actualOutput} instead`
+  `Expected test case 1 to be ${expectedOutput}, but got ${actualOutput} instead`
 );
 // Test case 2
 nums = [-7, -3, 2, 3, 11];
@@ -14,5 +14,5 @@ expectedOutput = [4, 9, 9, 49, 121];
 actualOutput = sortedSquares(nums);
 console.assert(
   JSON.stringify(actualOutput) === JSON.stringify(expectedOutput),
-  `Expected ${expectedOutput}, but got ${actualOutput} instead`
+  `Expected test case 2 to be${expectedOutput}, but got ${actualOutput} instead`
 );
