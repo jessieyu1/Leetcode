e0f591e2e72ee6e43842f637930885d2fae76529
Jessie <yujiaxin2009@gmail.com>
2024-11-20 20:23:29 +1100

    add edge cases

--- 0977-sorted-squares/0977-sortedSquares.js
+++ 0977-sorted-squares/0977-sortedSquares.js
@@ -1,19 +1,26 @@
 function sortedSquares(nums) {
+  if (!nums.length) return [];
+  if (nums.length === 1) return [nums[0] * nums[0]];
+
+  const result = new Array(nums.length);
   let left = 0;
   let right = nums.length - 1;
-  let newNum = [];
-  for (let i = nums.length - 1; i >= 0; i--) {
-    let square = 0;
-    if (Math.abs(nums[left]) < Math.abs(nums[right])) {
-      square = Math.abs(nums[right]);
-      right--;
-    } else {
-      square = Math.abs(nums[left]);
+  let position = nums.length - 1;
+  while (left <= right) {
+    const leftSquare = nums[left] * nums[left];
+    const rightSquare = nums[right] * nums[right];
+
+    if (leftSquare > rightSquare) {
+      result[position] = leftSquare;
       left++;
+    } else {
+      result[position] = rightSquare;
+      right--;
     }
-    newNum[i] = square * square;
+    position--;
   }
-  return newNum;
-};
+
+  return result;
+}
 
 module.exports = sortedSquares;
